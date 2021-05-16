dfc8d7a6e0c316940ac383e8be17c9828c622c2b
Michal Jablonski <michal.andrzej.jablonski@gmail.com>
2021-05-16 23:10:12 +0200

    remove task

--- js/script.js
+++ js/script.js
@@ -10,12 +10,17 @@
         },
     ];
 
+    const removeTask = (index) => {
+        nextYearTasks.splice(index, 1);
+        render();
+    }
+
     const addNewTask = (newTaskContent) => {
 
         nextYearTasks.push({
             content: newTaskContent,
         });
-    }
+    };
 
     const onFormSubmit = (event) => {
         event.preventDefault();
@@ -31,28 +36,37 @@
 
     };
 
+
     const render = () => {
         let htmlString = "";
 
         for (const nextYearTask of nextYearTasks) {
             htmlString += `
         <li class="${nextYearTask.done ? "taskDone" : ""}">
-        <button class="js-doneButton"></button>
+        <button class="js-doneButton">zrobione?</button>
         ${nextYearTask.content}
-        <button class="js-removeButton"></button>
+        <button class="js-removeButton">usuń</button>
         </li>
         `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-removeButton"); /* querySelectorAll zwraca pseudolistę dodanych przycisków po któych możemy iterować przy pomocy forEach */
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
     };
 
     const init = () => {
-        
+
         render();
 
         const form = document.querySelector(".js-form");
-        form.addEventListener("submit", onFormSubmit())
+        form.addEventListener("submit", onFormSubmit)
     };
 
     init();
