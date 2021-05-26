48321df25bd3413e7c54a95d1005a31a087aa468
Michal Jablonski <michal.andrzej.jablonski@gmail.com>
2021-05-26 02:55:41 +0200

    immutability refactoring and two additional buttons to hide done tasks and mark all tasks as done

--- js/script.js
+++ js/script.js
@@ -1,29 +1,47 @@
 {
-    const nextYearTasks = [
-        {
-            content: "zmiana samochodu",
-            done: true,
-        },
-        {
-            content: "remont mieszkania",
-            done: false,
-        },
-    ];
+    let nextYearTasks = [];
+    let hideDoneTasks = false;
 
     const removeTask = (index) => {
-        nextYearTasks.splice(index, 1);
+        /*nextYearTasks.splice(index, 1);//stara wersja z mutowaniem*/
+        nextYearTasks = [
+            ...nextYearTasks.slice(0, index),
+            ...nextYearTasks.slice(index + 1),
+        ];
         render();
     };
 
     const addNewTask = (newTaskContent) => {
-
-        nextYearTasks.push({
+        nextYearTasks = [
+            ...nextYearTasks,
+            { content: newTaskContent },
+        ];
+        render();
+        /*nextYearTasks.push({
             content: newTaskContent,
-        });
+        });  //stara wersja z mutowaniem*/
     };
 
     const toggleTaskDone = (index) => {
-        nextYearTasks[index].done = !nextYearTasks[index].done;
+        nextYearTasks = [
+            ...nextYearTasks.slice(0, index),
+            {
+                ...nextYearTasks[index],
+                done: !nextYearTasks[index].done
+            },
+            ...nextYearTasks.slice(index + 1),
+        ];
+        /*nextYearTasks[index].done = !nextYearTasks[index].done; stara wersja z mutowaniem*/
+        render();
+    };
+
+    const toggleHideDoneTask = () => {
+        hideDoneTasks = !hideDoneTasks;
+        render();
+    };
+
+    const finishAllTasks = () => {
+        nextYearTasks = nextYearTasks.map(task => ({...task, done: true}));
         render();
     };
 
@@ -41,7 +59,30 @@
         doneButtons.forEach((doneButton, index) => {
             doneButton.addEventListener("click", () => {
                 toggleTaskDone(index);
+
             });
+        });
+    };
+
+    const bindHideTasksEvent = () => {
+        const hideTasksButton = document.querySelector(".js-hideTasksButton");
+
+        if (!hideTasksButton) {
+            return;
+        }
+        hideTasksButton.addEventListener("click", () => {
+            toggleHideDoneTask();
+        });
+    };
+
+    const bindFinishAllTasksEvent = () => {
+        const finishTasksButton = document.querySelector(".js-tasksDoneButton");
+
+        if (!finishTasksButton) {
+            return;
+        }
+        finishTasksButton.addEventListener("click", () => {
+            finishAllTasks();
         });
     };
 
@@ -54,7 +95,7 @@
         if (newTaskContent !== "") {
             addNewTask(newTaskContent);
             newTaskElement.value = "";
-        }
+        };
 
         render();
 
@@ -62,24 +103,44 @@
 
     };
 
-    const render = () => {
+    const renderTasks = () => {
+
         let htmlString = "";
 
         for (const nextYearTask of nextYearTasks) {
             htmlString += `
-        <li type="none" class="listItem 
-            ${nextYearTask.done ? "taskDone" : ""}
-            ">
-                <button class="button js-doneButton"></button>
-                <span class="newItemText">${nextYearTask.content} </span>
+            <li type="none" class="listItem ${nextYearTask.done && hideDoneTasks ? "listItem--hide" : ""}"> 
+                <button class="button js-doneButton ${nextYearTask.done ? "button--thick" : ""}"></button>
+                    <span class="newItemText ${nextYearTask.done ? "taskDone" : ""}">${nextYearTask.content} </span>
                 <button class="button button--remove js-removeButton"></button>
-        </li>
-        `;
-        }
+            </li>
+            `;
+        };
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const renderButtons = () => {
+        let htmlButtons = "";
+
+        if (nextYearTasks.length > 0) {
+            htmlButtons +=
+                `<button class="js-hideTasksButton taskList__button">${hideDoneTasks ? "Pokaż ukończone" : "Ukryj ukończone"}</button>
+                <button class="js-tasksDoneButton taskList__button" ${nextYearTasks.every(task => task.done) ? "disabled" : ""}>Ukończ wszystkie</button>`
+        } else {
+            htmlButtons = "";
+        };
+
+        document.querySelector(".taskList__buttons").innerHTML = htmlButtons;
+    };
+
+    const render = () => {
+        renderTasks();
+        renderButtons();
 
         bindEvents();
+        bindHideTasksEvent();
+        bindFinishAllTasksEvent();
     };
 
     const init = () => {
